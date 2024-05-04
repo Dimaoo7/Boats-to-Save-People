156d07cbd43ac104405cd6e7240cb26dbaca00fe
Дмитрий <dimakrjukov@gmail.com>
2024-05-04 13:42:24 +0200

    Runtime 16 ms Beats 97.17% of users with Java

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,50 @@
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] people = new int[]{3,5,3,4};
         int limit = 5;
+
+        System.out.println(numRescueBoats(people,limit));
+
+    }
+
+
+    public static int numRescueBoats(int[] people, int limit) {
         int boats = 0;
-
-    }
-    public static int[] sortEd(int[] people) {
-        boolean isSorted;
-        do {
-            isSorted = true;
-            int temp;
-            for (int i = 1; i < people.length; i++) {
-                if (people[i -1] > people[i]) {
-                    temp = people[i - 1];
-                    people[i - 1] = people[i];
-                    people[i] = temp;
-                    isSorted = false;
-                }
-
+        Arrays.sort(people);
+        int i=0,j=people.length-1;
+        while(i<=j){
+            if((people[j]+people[i])<=limit){
+                i++;
             }
-
+            j--;
+            boats++;
         }
-        while (isSorted);
-
-        return people;
+        return boats;
     }
+//    public static int[] sortEd(int[] people) {
+//        boolean isSorted;
+//        do {
+//            isSorted = true;
+//            int temp;
+//            for (int i = 1; i < people.length; i++) {
+//                if (people[i -1] > people[i]) {
+//                    temp = people[i - 1];
+//                    people[i - 1] = people[i];
+//                    people[i] = temp;
+//                    isSorted = false;
+//                }
+//
+//            }
+//
+//        }
+//        while (isSorted);
+//
+//        return people;
+//    }
 }
 
-
+//
 
