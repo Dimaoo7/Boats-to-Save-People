07ad2af67a8fe81877ab5ffd2f49a112bf11bb10
Дмитрий <dimakrjukov@gmail.com>
2024-05-17 10:53:59 +0200

    2706. Buy Two Chocolates

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] prices = new int[]{1,2,2};
+        int[] prices = new int[]{1, 2, 2};
         int money = 3;
         System.out.println(buyChoco(prices, money));
 
     }
+//    You are given an integer array prices representing the prices of various chocolates
+//     in a store. You are also given a single integer money, which represents your initial amount of money.
+//
+//    You must buy exactly two chocolates in such a way that you still have some
+//     non-negative leftover money. You would like to minimize the sum of the prices of the two chocolates you buy.
+//
+//    Return the amount of money you will have leftover after buying the two chocolates.
+//     If there is no way for you to buy two chocolates without ending up in debt, return money.
+//     Note that the leftover must be non-negative.
+
 
     private static int buyChoco(int[] prices, int money) {
 
